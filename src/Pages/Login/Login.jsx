9695b8311e94e625ee36644c3f7ceeeca962c79b
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2024-10-06 02:39:56 +0300

    login design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import './login.css'
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 export default function Login() {
+
+    let validationScheama=Yup.object({
+          email: Yup.string().email('Enter A valid Email').required('Email is required'),
+          password:Yup.string().required('Password is required')
+         
+       });
+      function hiLogi() {
+        window.alert('hi login')
+      }
+    const formik = useFormik({
+      initialValues:{
+         email:'',
+         password:''
+      },validationSchema:validationScheama,
+      onSubmit:hiLogi
+    })
     return (
         < >
             <div className='div'>
-                <div className="background">
-                    <div className="shape" />
-                    <div className="shape" />
-                </div>
-                <form>
-                    <h3>Login Here</h3>
+              
+                <form onSubmit={formik.handleSubmit}>
+                    <h3>Login</h3>
                     <label htmlFor="Email" className='text-light'>Email</label>
-                    <input type="Email" placeholder="Email  " id="Email" />
+                    <input type="email" placeholder="Email " onBlur={formik.handleChange} onChange={formik.values.email}  name='email' id="Email" />
+                    <h6 className='text-danger mt-3'>{formik.errors?.email}</h6> 
                     <label htmlFor="password" className='text-light'>Password</label>
-                    <input type="password" placeholder="Password" id="password" />
+                    <input type="password" onBlur={formik.handleChange} placeholder="Password"  onChange={formik.values.password} name='password' id="password" />
+                    <h6 className='text-danger mt-3'>{formik.errors?.password}</h6>
                     <button className='mt-5'>Log In</button>
                    
                 </form>
